13872ddc380a88d260bc3a4fbcc50affbd07e59e
Actine <actinate@gmail.com>
2015-09-23 20:31:12 +0300

    Дописав лістинг про оператори. Дописав деякі відомості у лістинг про змінні. Прилизав гру на впізнавання числа і переніс її у пакет samples

---
+++ src/com/uzhnu/training/samples/RandomGame.java
@@ -0,0 +1,38 @@
+package com.uzhnu.training.samples;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Гра, де комп’ютер загадує число від 0 до 100, і його треба відгадати
+ *
+ * @author Paul Danyliuk
+ * @date 15:52 @ 23.09.2015.
+ */
+public class RandomGame {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = new Random().nextInt(101);      // ціле у проміжку [0, 101), тобто [0, 100]
+        int response;
+
+        System.out.println("Я задумав число від 0 до 100, спробуйте відгадати його");
+
+        while (true) {
+            // Зчитати введений рядок із консолі і розпізнати в ньому ціле число
+            response = Integer.parseInt(scanner.nextLine());
+
+            if (response == number) {
+                System.out.println("Ви відгадали!");
+                break;
+            } else if (response < number) {
+                System.out.println("Ні, спробуйте більше число");
+            } else {
+                System.out.println("Ні, спробуйте менше число");
+            }
+        }
+    }
+
+}
