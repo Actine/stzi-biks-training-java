331daa18218ca9848344ef77dae90ffd1aabdad6
Actine <actinate@gmail.com>
2015-09-26 02:36:49 +0300

    Полагодив нескінченний цикл

--- src/com/uzhnu/training/samples/basics/FlowControlsApp.java
+++ src/com/uzhnu/training/samples/basics/FlowControlsApp.java
@@ -403,7 +403,7 @@ public class FlowControlsApp {
         if (true) {
             int definedInIfStatement = 10;
         }
-        for (int j = 0; i < 3; j++) {
+        for (int j = 0; j < 3; j++) {
             int definedInLoop = 20;
         }
 
